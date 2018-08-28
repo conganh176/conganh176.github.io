98e750615381525785a25603531840666731bcce
conganh176 <conganh176@users.noreply.github.com>
2018-08-28 17:41:37 +0700

    BT23

--- BT23/js/script.js
+++ BT23/js/script.js
@@ -1,28 +1,21 @@
 function playSound(hoveraudio) {
-				var audio1=document.getElementById(hoveraudio);
-				audio1.play();
-			}
+	var audio1=document.getElementById(hoveraudio);
+	audio1.play();
+}
 
 function stopSound(hoveraudio) {
-				var audio1=document.getElementById(hoveraudio);
-				audio1.pause();
-				audio1.currentTime = 0;
-			}
+	var audio1=document.getElementById(hoveraudio);
+	audio1.pause();
+	audio1.currentTime = 0;
+}
 
 function play(clickaudio) {
-				var audio2=document.getElementById(clickaudio);
-				audio2.play();
-			}
+	var audio2=document.getElementById(clickaudio);
+	audio2.play();
+}
 
 document.getElementById("playpause").onclick = function() {
-					    var audio = document.getElementById("audio");
-					    if (audio.paused) audio.play();
-					    else audio.pause();
-					};
-
-document.getElementById("playpause2").onclick = function() {
-					    var audio = document.getElementById("audio");
-					    if (audio.paused) audio.play();
-					    else audio.pause();
-					};
-
+    var audio = document.getElementById("background");
+    if (audio.paused) audio.play();
+    else audio.pause();
+};
