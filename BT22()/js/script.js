f3626cc2c3a1e634cad13f76f89141e446246988
conganh176 <conganh176@users.noreply.github.com>
2018-09-03 18:16:28 +0700

    BT22

--- BT22()/js/script.js
+++ BT22()/js/script.js
@@ -1,7 +1,7 @@
 var singer=[
 
 {
-	image:"vy1.png",
+	image:"vy1",
 	name:"VY1",
 	sex:"Female",
 	developer:"Yamaha Corporation",
@@ -10,7 +10,7 @@ var singer=[
 },
 
 {
-	image:"diva.png",
+	image:"diva",
 	name:"Cyber Diva",
 	sex:"Female",
 	developer:"Yamaha Corporation",
@@ -19,7 +19,7 @@ var singer=[
 },
 
 {
-	image:"yuzuki.png",
+	image:"yuzuki",
 	name:"Yuzuki Yukari",
 	sex:"Female",
 	developer:"AH-Software",
@@ -29,7 +29,7 @@ var singer=[
 
 
 {
-	image:"luka.png",
+	image:"luka",
 	name:"Megurine Luka",
 	sex:"Female",
 	developer:"Crypton Future Media",
@@ -38,7 +38,7 @@ var singer=[
 },
 
 {
-	image:"gakupo.png",
+	image:"gakupo",
 	name:"Gackpoid",
 	sex:"Male",
 	developer:"Internet Co. Ltd.",
@@ -47,7 +47,7 @@ var singer=[
 },
 
 {
-	image:"miki.jpg",
+	image:"miki",
 	name:"Miki",
 	sex:"Female",
 	developer:"AH-Software",
@@ -56,7 +56,7 @@ var singer=[
 },
 
 {
-	image:"nekomura.png",
+	image:"nekomura",
 	name:"Nekomura Iroha",
 	sex:"Female",
 	developer:"AH-Software",
@@ -65,16 +65,16 @@ var singer=[
 },
 
 {
-	image:"flower.png",
+	image:"flower",
 	name:"Flower",
 	sex:"Female",
-	developer:"Gynoid Co., Ltd. & Yamaha Corporation",
+	developer:"Gynoid Co., Ltd. <br/>& Yamaha Corporation",
 	language:"Japanese",
 	voice:"Unknown"
 },
 
 {
-	image:"sachiko.png",
+	image:"sachiko",
 	name:"Sachiko",
 	sex:"Female",
 	developer:"Yamaha Corporation",
@@ -83,16 +83,16 @@ var singer=[
 },
 
 {
-	image:"arsloid.png",
+	image:"arsloid",
 	name:"Arsloid",
 	sex:"Male",
-	developer:"Universal Music Japan & Yamaha Corporation",
+	developer:"Universal Music Japan <br/>& Yamaha Corporation",
 	language:"Japanese",
 	voice:"Akira Kano"
 },
 
 {
-	image:"ruby.jpg",
+	image:"ruby",
 	name:"Ruby",
 	sex:"Female",
 	developer:"VocaTone/PowerFX",
@@ -101,7 +101,7 @@ var singer=[
 },
 
 {
-	image:"kaai.jpg",
+	image:"kaai",
 	name:"Kaai Yuki",
 	sex:"Female",
 	developer:"AH-Software",
@@ -110,7 +110,7 @@ var singer=[
 },
 
 {
-	image:"hiyama.png",
+	image:"hiyama",
 	name:"Hiyama Kiyoteru",
 	sex:"Male",
 	developer:"AH-Software",
@@ -119,7 +119,7 @@ var singer=[
 },
 
 {
-	image:"megpoid.jpg",
+	image:"megpoid",
 	name:"Megpoid",
 	sex:"Female",
 	developer:"Internet Co. Ltd.",
@@ -128,7 +128,7 @@ var singer=[
 },
 
 {
-	image:"dex.png",
+	image:"dex",
 	name:"Dex",
 	sex:"Male",
 	developer:"Zero-G",
@@ -137,7 +137,7 @@ var singer=[
 },
 
 {
-	image:"daina.png",
+	image:"daina",
 	name:"Daina",
 	sex:"Female",
 	developer:"Zero-G",
@@ -146,7 +146,7 @@ var singer=[
 },
 
 {
-	image:"rana.png",
+	image:"rana",
 	name:"Rana",
 	sex:"Female",
 	developer:"We've Inc.",
@@ -155,70 +155,70 @@ var singer=[
 },
 
 {
-	image:"rin.png",
+	image:"rin",
 	name:"Rin",
 	sex:"Female",
 	developer:"Crypton Future Media",
-	language:"English & Japanese",
+	language:"English <br/>& Japanese",
 	voice:"Asami Shimoda"
 },
 
 {
-	image:"len.jpg",
+	image:"len",
 	name:"Len",
 	sex:"Male",
 	developer:"Crypton Future Media",
-	language:"English & Japanese",
+	language:"English <br/>& Japanese",
 	voice:"Asami Shimoda"
 },
 
 {
-	image:"unity.png",
+	image:"unity",
 	name:"Unity",
 	sex:"Female",
-	developer:"Unity Technologies Japan & Yamaha Corporation",
+	developer:"Unity Technologies Japan <br/>& Yamaha Corporation",
 	language:"Japanese",
 	voice:"Asuka Kakumoto"
 },
 
 {
-	image:"fukase.png",
+	image:"fukase",
 	name:"Fukase",
 	sex:"Male",
 	developer:"Yamaha Corporation",
-	language:"English & Japanese",
+	language:"English <br/>& Japanese",
 	voice:"Satoshi Fukase"
 },
 
 {
-	image:"xingchen.png",
+	image:"xingchen",
 	name:"Xingchen",
 	sex:"Female",
-	developer:"Beijing Photek S&T & Shanghai Henian",
+	developer:"Beijing Photek S&T <br/>& Shanghai Henian",
 	language:"Chinese",
 	voice:"Chalili"
 },
 
 {
-	image:"otomachi.jpg",
+	image:"otomachi",
 	name:"Otomachi Una",
 	sex:"Female",
-	developer:"Internet Co., Ltd & MTK",
+	developer:"Internet Co., Ltd <br/>& MTK",
 	language:"Japanese",
 	voice:"Aimi Tanaka"
 },
 
 {
-	image:"miku.png",
+	image:"miku",
 	name:"Hatsune Miku",
 	sex:"Female",
 	developer:"Crypton Future Media",
-	language:"Chinese, English & Japanese",
+	language:"Chinese, English <br/>& Japanese",
 	voice:"Saki Fujita"
 },
 
 {
-	image:"tohoku.png",
+	image:"tohoku",
 	name:"Tohoku Zunko",
 	sex:"Female",
 	developer:"AH-Software",
@@ -227,7 +227,7 @@ var singer=[
 },
 
 {
-	image:"songman.jpg",
+	image:"songman",
 	name:"Cyber Songman",
 	sex:"Male",
 	developer:"Yamaha Corporation",
@@ -236,16 +236,16 @@ var singer=[
 },
 
 {
-	image:"macne.jpg",
+	image:"macne",
 	name:"Macne Nana",
 	sex:"Female",
 	developer:"MI7",
-	language:"English & Japanese",
+	language:"English <br/>& Japanese",
 	voice:"Haruna Ikezawa"
 },
 
 {
-	image:"uni.png",
+	image:"uni",
 	name:"Uni",
 	sex:"Female",
 	developer:"ST Media",
@@ -254,7 +254,7 @@ var singer=[
 },
 
 {
-	image:"tone.png",
+	image:"tone",
 	name:"Tone Rion",
 	sex:"Female",
 	developer:"Dear Stage (Moe Japan)",
@@ -263,7 +263,7 @@ var singer=[
 },
 
 {
-	image:"yumemi.png",
+	image:"yumemi",
 	name:"Yumemi Nemu",
 	sex:"Female",
 	developer:"Dear Stage (Moe Japan)",
@@ -272,7 +272,7 @@ var singer=[
 },
 
 {
-	image:"yuezheng.png",
+	image:"yuezheng",
 	name:"Yuezheng Longya",
 	sex:"Male",
 	developer:"Shanghai Henian",
@@ -281,25 +281,25 @@ var singer=[
 },
 
 {
-	image:"azuki.png",
+	image:"azuki",
 	name:"Azuki",
 	sex:"Female",
-	developer:"Sega & Yamaha Corporation",
+	developer:"Sega <br/>& Yamaha Corporation",
 	language:"Japanese",
 	voice:"Yuka Ōtsubo"
 },
 
 {
-	image:"matcha.png",
+	image:"matcha",
 	name:"Matcha",
 	sex:"Female",
-	developer:"Sega & Yamaha Corporation",
+	developer:"Sega <br/>& Yamaha Corporation",
 	language:"Japanese",
 	voice:"Ayaka Ohashi"
 },
 
 {
-	image:"lumi.jpg",
+	image:"lumi",
 	name:"Lumi",
 	sex:"Female",
 	developer:"Akatsuki Virtual Artists",
@@ -308,34 +308,25 @@ var singer=[
 },
 
 {
-	image:"xinhua.jpg",
+	image:"xinhua",
 	name:"Xin Hua",
 	sex:"Female",
-	developer:"Gynoid Co., Ltd. & Shanghai Wangcheng",
-	language:"Chinese",
+	developer:"Gynoid Co., Ltd. <br/>& Shanghai Wangcheng",
+	language:"Chinese <br/>& Japanese",
 	voice:"Wenyi Wang"
 },
 
 {
-	image:"xinhua.jpg",
-	name:"Xin Hua",
-	sex:"Female",
-	developer:"Gynoid Co., Ltd. & Shanghai Wangcheng",
-	language:"Chinese & Japanese",
-	voice:"Wenyi Wang"
-},
-
-{
-	image:"luo.png",
+	image:"luo",
 	name:"Luo Tianyi",
 	sex:"Female",
 	developer:"Shanghai Henian",
-	language:"Chinese & Japanese",
+	language:"Chinese <br/>& Japanese",
 	voice:"Shan Xin"
 },
 
 {
-	image:"kizuna.png",
+	image:"kizuna",
 	name:"Kizuna Akari",
 	sex:"Female",
 	developer:"AH-Software",
@@ -344,16 +335,16 @@ var singer=[
 },
 
 {
-	image:"mirai.png",
+	image:"mirai",
 	name:"Mirai Komachi",
 	sex:"Female",
-	developer:"Bandai Namco Studio, Inc & Yamaha Corporation",
+	developer:"Bandai Namco Studio, Inc <br/>& Yamaha Corporation",
 	language:"Japanese",
 	voice:"Unknown"
 },
 
 {
-	image:"zhiyu.png",
+	image:"zhiyu",
 	name:"Zhiyu Moke",
 	sex:"Male",
 	developer:"Shanghai Henian",
@@ -362,7 +353,7 @@ var singer=[
 },
 
 {
-	image:"moqingxian.png",
+	image:"moqingxian",
 	name:"Mo Qingxian",
 	sex:"Female",
 	developer:"Shanghai Henian",
@@ -371,3 +362,80 @@ var singer=[
 },
 
 ]
+
+for (var i=0; i<singer.length; i++) {
+	$('tbody').append('<tr>'+
+			'<td class="image"><img src="img/'+singer[i].image+'.png" alt="Singer '+(i+1)+'"></td>'+
+			'<td>'+singer[i].name+'</td>'+
+			'<td>'+singer[i].sex+'</td>'+
+			'<td>'+singer[i].developer+'</td>'+
+			'<td>'+singer[i].language+'</td>'+
+			'<td>'+singer[i].voice+'</td>'+
+		'</tr>');
+}
+
+function sortTable(n) {
+  var table, rows, switching, i, x, y, shouldSwitch, dir, switchcount = 0;
+  table = document.getElementById("table");
+  switching = true;
+  //Set the sorting direction to ascending:
+  dir = "asc"; 
+  rows = table.rows;
+  /*Make a loop that will continue until
+  no switching has been done:*/
+  for (var i = 1; i < table.rows[0].getElementsByTagName("TH").length; i++) {
+	  rows[0].getElementsByTagName("TH")[i].querySelector('i').classList.remove("fa-sort-up");
+	  rows[0].getElementsByTagName("TH")[i].querySelector('i').classList.remove("fa-sort-down");
+	  rows[0].getElementsByTagName("TH")[i].querySelector('i').classList.add("fa-sort");
+  }
+  while (switching) {
+    //start by saying: no switching is done:
+    switching = false;
+    
+    /*Loop through all table rows (except the
+    first, which contains table headers):*/
+    for (i = 1; i < (rows.length - 1); i++) {
+      //start by saying there should be no switching:
+      shouldSwitch = false;
+      /*Get the two elements you want to compare,
+      one from current row and one from the next:*/
+      x = rows[i].getElementsByTagName("TD")[n];
+      y = rows[i + 1].getElementsByTagName("TD")[n];
+      /*check if the two rows should switch place,
+      based on the direction, asc or desc:*/
+      if (dir == "asc") {
+        if (x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase()) {
+          //if so, mark as a switch and break the loop:
+          shouldSwitch= true;
+          rows[0].getElementsByTagName("TH")[n].querySelector('i').classList.remove("fa-sort");
+          rows[0].getElementsByTagName("TH")[n].querySelector('i').classList.add("fa-sort-up");
+          break;
+        }
+      } else if (dir == "desc") {
+        if (x.innerHTML.toLowerCase() < y.innerHTML.toLowerCase()) {
+          //if so, mark as a switch and break the loop:
+          shouldSwitch = true;
+          rows[0].getElementsByTagName("TH")[n].querySelector('i').classList.remove("fa-sort");
+          rows[0].getElementsByTagName("TH")[n].querySelector('i').classList.remove("fa-sort-up");
+          rows[0].getElementsByTagName("TH")[n].querySelector('i').classList.add("fa-sort-down");
+          break;
+        }
+      }
+    }
+    if (shouldSwitch) {
+      /*If a switch has been marked, make the switch
+      and mark that a switch has been done:*/
+      rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
+      switching = true;
+      //Each time a switch is done, increase this count by 1:
+      switchcount ++;      
+    } else {
+      /*If no switching has been done AND the direction is "asc",
+      set the direction to "desc" and run the while loop again.*/
+      if (switchcount == 0 && dir == "asc") {
+        dir = "desc";
+        switching = true;
+      }
+    }
+  }
+}
