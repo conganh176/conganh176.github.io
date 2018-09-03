7f4963e3d0c4b61d4b53387196aa1f5b20c0e287
conganh176 <conganh176@users.noreply.github.com>
2018-09-03 12:12:30 +0700

    BT22

---
+++ BT22()/js/script.js
@@ -0,0 +1,373 @@
+var singer=[
+
+{
+	image:"vy1.png",
+	name:"VY1",
+	sex:"Female",
+	developer:"Yamaha Corporation",
+	language:"Japanese",
+	voice:"Unknown"
+},
+
+{
+	image:"diva.png",
+	name:"Cyber Diva",
+	sex:"Female",
+	developer:"Yamaha Corporation",
+	language:"English",
+	voice:"Jenny Shima"
+},
+
+{
+	image:"yuzuki.png",
+	name:"Yuzuki Yukari",
+	sex:"Female",
+	developer:"AH-Software",
+	language:"Japanese",
+	voice:"Chihiro Ishiguro"
+},
+
+
+{
+	image:"luka.png",
+	name:"Megurine Luka",
+	sex:"Female",
+	developer:"Crypton Future Media",
+	language:"Japanese",
+	voice:"Yū Asakawa"
+},
+
+{
+	image:"gakupo.png",
+	name:"Gackpoid",
+	sex:"Male",
+	developer:"Internet Co. Ltd.",
+	language:"Japanese",
+	voice:"Gackt"
+},
+
+{
+	image:"miki.jpg",
+	name:"Miki",
+	sex:"Female",
+	developer:"AH-Software",
+	language:"Japanese",
+	voice:"Miki Furukawa"
+},
+
+{
+	image:"nekomura.png",
+	name:"Nekomura Iroha",
+	sex:"Female",
+	developer:"AH-Software",
+	language:"Japanese",
+	voice:"Kyounosuke Yoshitate"
+},
+
+{
+	image:"flower.png",
+	name:"Flower",
+	sex:"Female",
+	developer:"Gynoid Co., Ltd. & Yamaha Corporation",
+	language:"Japanese",
+	voice:"Unknown"
+},
+
+{
+	image:"sachiko.png",
+	name:"Sachiko",
+	sex:"Female",
+	developer:"Yamaha Corporation",
+	language:"Japanese",
+	voice:"Sachiko Kobayashi"
+},
+
+{
+	image:"arsloid.png",
+	name:"Arsloid",
+	sex:"Male",
+	developer:"Universal Music Japan & Yamaha Corporation",
+	language:"Japanese",
+	voice:"Akira Kano"
+},
+
+{
+	image:"ruby.jpg",
+	name:"Ruby",
+	sex:"Female",
+	developer:"VocaTone/PowerFX",
+	language:"English",
+	voice:"Mishakeet"
+},
+
+{
+	image:"kaai.jpg",
+	name:"Kaai Yuki",
+	sex:"Female",
+	developer:"AH-Software",
+	language:"Japanese",
+	voice:"Unknown"
+},
+
+{
+	image:"hiyama.png",
+	name:"Hiyama Kiyoteru",
+	sex:"Male",
+	developer:"AH-Software",
+	language:"Japanese",
+	voice:"Kiyoshi Hiyama"
+},
+
+{
+	image:"megpoid.jpg",
+	name:"Megpoid",
+	sex:"Female",
+	developer:"Internet Co. Ltd.",
+	language:"Japanese",
+	voice:"Megumi Nakajima"
+},
+
+{
+	image:"dex.png",
+	name:"Dex",
+	sex:"Male",
+	developer:"Zero-G",
+	language:"English",
+	voice:"Sam Blakeslee"
+},
+
+{
+	image:"daina.png",
+	name:"Daina",
+	sex:"Female",
+	developer:"Zero-G",
+	language:"English",
+	voice:"Aki Glancy"
+},
+
+{
+	image:"rana.png",
+	name:"Rana",
+	sex:"Female",
+	developer:"We've Inc.",
+	language:"Japanese",
+	voice:"Ai Kakuma"
+},
+
+{
+	image:"rin.png",
+	name:"Rin",
+	sex:"Female",
+	developer:"Crypton Future Media",
+	language:"English & Japanese",
+	voice:"Asami Shimoda"
+},
+
+{
+	image:"len.jpg",
+	name:"Len",
+	sex:"Male",
+	developer:"Crypton Future Media",
+	language:"English & Japanese",
+	voice:"Asami Shimoda"
+},
+
+{
+	image:"unity.png",
+	name:"Unity",
+	sex:"Female",
+	developer:"Unity Technologies Japan & Yamaha Corporation",
+	language:"Japanese",
+	voice:"Asuka Kakumoto"
+},
+
+{
+	image:"fukase.png",
+	name:"Fukase",
+	sex:"Male",
+	developer:"Yamaha Corporation",
+	language:"English & Japanese",
+	voice:"Satoshi Fukase"
+},
+
+{
+	image:"xingchen.png",
+	name:"Xingchen",
+	sex:"Female",
+	developer:"Beijing Photek S&T & Shanghai Henian",
+	language:"Chinese",
+	voice:"Chalili"
+},
+
+{
+	image:"otomachi.jpg",
+	name:"Otomachi Una",
+	sex:"Female",
+	developer:"Internet Co., Ltd & MTK",
+	language:"Japanese",
+	voice:"Aimi Tanaka"
+},
+
+{
+	image:"miku.png",
+	name:"Hatsune Miku",
+	sex:"Female",
+	developer:"Crypton Future Media",
+	language:"Chinese, English & Japanese",
+	voice:"Saki Fujita"
+},
+
+{
+	image:"tohoku.png",
+	name:"Tohoku Zunko",
+	sex:"Female",
+	developer:"AH-Software",
+	language:"Japanese",
+	voice:"Satomi Sato"
+},
+
+{
+	image:"songman.jpg",
+	name:"Cyber Songman",
+	sex:"Male",
+	developer:"Yamaha Corporation",
+	language:"English",
+	voice:"Unknown"
+},
+
+{
+	image:"macne.jpg",
+	name:"Macne Nana",
+	sex:"Female",
+	developer:"MI7",
+	language:"English & Japanese",
+	voice:"Haruna Ikezawa"
+},
+
+{
+	image:"uni.png",
+	name:"Uni",
+	sex:"Female",
+	developer:"ST Media",
+	language:"Korean",
+	voice:"Unknown"
+},
+
+{
+	image:"tone.png",
+	name:"Tone Rion",
+	sex:"Female",
+	developer:"Dear Stage (Moe Japan)",
+	language:"Japanese",
+	voice:"Nemu Yumemi"
+},
+
+{
+	image:"yumemi.png",
+	name:"Yumemi Nemu",
+	sex:"Female",
+	developer:"Dear Stage (Moe Japan)",
+	language:"Japanese",
+	voice:"Nemu Yumemi"
+},
+
+{
+	image:"yuezheng.png",
+	name:"Yuezheng Longya",
+	sex:"Male",
+	developer:"Shanghai Henian",
+	language:"Chinese",
+	voice:"Jie Zhang"
+},
+
+{
+	image:"azuki.png",
+	name:"Azuki",
+	sex:"Female",
+	developer:"Sega & Yamaha Corporation",
+	language:"Japanese",
+	voice:"Yuka Ōtsubo"
+},
+
+{
+	image:"matcha.png",
+	name:"Matcha",
+	sex:"Female",
+	developer:"Sega & Yamaha Corporation",
+	language:"Japanese",
+	voice:"Ayaka Ohashi"
+},
+
+{
+	image:"lumi.jpg",
+	name:"Lumi",
+	sex:"Female",
+	developer:"Akatsuki Virtual Artists",
+	language:"Japanese",
+	voice:"Sayaka Ohara"
+},
+
+{
+	image:"xinhua.jpg",
+	name:"Xin Hua",
+	sex:"Female",
+	developer:"Gynoid Co., Ltd. & Shanghai Wangcheng",
+	language:"Chinese",
+	voice:"Wenyi Wang"
+},
+
+{
+	image:"xinhua.jpg",
+	name:"Xin Hua",
+	sex:"Female",
+	developer:"Gynoid Co., Ltd. & Shanghai Wangcheng",
+	language:"Chinese & Japanese",
+	voice:"Wenyi Wang"
+},
+
+{
+	image:"luo.png",
+	name:"Luo Tianyi",
+	sex:"Female",
+	developer:"Shanghai Henian",
+	language:"Chinese & Japanese",
+	voice:"Shan Xin"
+},
+
+{
+	image:"kizuna.png",
+	name:"Kizuna Akari",
+	sex:"Female",
+	developer:"AH-Software",
+	language:"Japanese",
+	voice:"Madoka Yonezawa"
+},
+
+{
+	image:"mirai.png",
+	name:"Mirai Komachi",
+	sex:"Female",
+	developer:"Bandai Namco Studio, Inc & Yamaha Corporation",
+	language:"Japanese",
+	voice:"Unknown"
+},
+
+{
+	image:"zhiyu.png",
+	name:"Zhiyu Moke",
+	sex:"Male",
+	developer:"Shanghai Henian",
+	language:"Chinese",
+	voice:"Shangqing Su"
+},
+
+{
+	image:"moqingxian.png",
+	name:"Mo Qingxian",
+	sex:"Female",
+	developer:"Shanghai Henian",
+	language:"Chinese",
+	voice:"Mingyue"
+},
+
+]
