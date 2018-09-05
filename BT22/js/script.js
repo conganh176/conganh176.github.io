132c6416add92e6620c1b16f4393c1e2a3a10b96
conganh176 <conganh176@users.noreply.github.com>
2018-09-05 09:30:08 +0700

    BT22

--- BT22/js/script.js
+++ BT22/js/script.js
@@ -367,7 +367,7 @@ var audio=["vy1", "diva", "yuzuki", "luka", "gakupo", "miki", "nekomura", "flowe
 		"megpoid", "dex", "daina", "rana", "rin", "len", "unity", "fukase", "xingchen", "otomachi", "miku", "tohoku", "songman", "macne",
 		"uni", "tone", "yumemi", "yuezheng", "azuki", "matcha", "lumi", "xinhua", "luo", "kizuna", "mirai", "zhiyu", "moqingxian"];
 
-// var myAudio = $('#background')[0];
+var myAudio = $('#background')[0];
 
 for (var i=0; i<singer.length; i++) {
 	$('tbody').append('<tr onmouseenter="$(\'#hover\')[0].play()" onmouseout="$(\'#hover\')[0].pause();$(\'#hover\')[0].currentTime=0">'+
@@ -456,61 +456,63 @@ function sortTable(n) {
   }
 }
 
-// window.onload = function() {
+function loadmusic() {
 
 	
-// 	var context = new (window.AudioContext || window.webkitAudioContext)();
-// 	var src = context.createMediaElementSource(myAudio);
-// 	var analyser = context.createAnalyser();
+	var context = new (window.AudioContext || window.webkitAudioContext)();
+	var src = context.createMediaElementSource(myAudio);
+	var analyser = context.createAnalyser();
 
-// 	var canvas = document.getElementById("canvas");
-// 	    canvas.width = window.innerWidth;
-// 	    canvas.height = window.innerHeight;
-// 	var ctx = canvas.getContext("2d");
+	var canvas = document.getElementById("canvas");
+	    canvas.width = window.innerWidth;
+	    canvas.height = window.innerHeight;
+	var ctx = canvas.getContext("2d");
 
-// 	    src.connect(analyser);
-// 	    analyser.connect(context.destination);
+	    src.connect(analyser);
+	    analyser.connect(context.destination);
 
-// 	    analyser.fftSize = 256;
+	    analyser.fftSize = 256;
 
-// 	var bufferLength = analyser.frequencyBinCount;
-// 	    console.log(bufferLength);
+	var bufferLength = analyser.frequencyBinCount;
+	    console.log(bufferLength);
 
-// 	var dataArray = new Uint8Array(bufferLength);
+	var dataArray = new Uint8Array(bufferLength);
 
-// 	var WIDTH = canvas.width;
-// 	var HEIGHT = canvas.height;
+	var WIDTH = canvas.width;
+	var HEIGHT = canvas.height;
 
-// 	var barWidth = (WIDTH / bufferLength) * 2.5;
-// 	var barHeight;
-// 	var x = 0;
+	var barWidth = (WIDTH / bufferLength) * 2.5;
+	var barHeight;
+	var x = 0;
 
-// 	function renderFrame() {
-// 	    requestAnimationFrame(renderFrame);
+	function renderFrame() {
+	    requestAnimationFrame(renderFrame);
 
-// 	    x = 0;
+	    x = 0;
 
-// 	    analyser.getByteFrequencyData(dataArray);
+	    analyser.getByteFrequencyData(dataArray);
 
-// 	    ctx.fillStyle = "#000";
-// 	    ctx.fillRect(0, 0, WIDTH, HEIGHT);
+	    ctx.fillStyle = "#000";
+	    ctx.fillRect(0, 0, WIDTH, HEIGHT);
 
-// 	    for (var i = 0; i < bufferLength; i++) {
-// 		    barHeight = dataArray[i];
+	    for (var i = 0; i < bufferLength; i++) {
+		    barHeight = dataArray[i];
 	        
-// 	        var r = barHeight + (25 * (i/bufferLength));
-// 	        var g = 250 * (i/bufferLength);
-// 	        var b = 50;
+	        var r = barHeight + (25 * (i/bufferLength));
+	        var g = 250 * (i/bufferLength);
+	        var b = 50;
 
-// 	        ctx.fillStyle = "rgb(" + r + "," + g + "," + b + ")";
-// 	        ctx.fillRect(x, HEIGHT - barHeight, barWidth, barHeight);
+	        ctx.fillStyle = "rgb(" + r + "," + g + "," + b + ")";
+	        ctx.fillRect(x, HEIGHT - barHeight, barWidth, barHeight);
 
-// 	        x += barWidth + 1;
-// 	      }
-// 	}
+	        x += barWidth + 1;
+	      }
+	}
 
-// 	renderFrame();
+	renderFrame();
 
-// };
+};
 
-// audio.crossOrigin = 'anonymous';
+loadmusic();
+
+audio.crossOrigin="anonymous";
