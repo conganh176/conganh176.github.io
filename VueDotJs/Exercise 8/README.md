0efaa34bd0aa87c0ab77f39312aafa0e81a1cb7a
conganh176 <conganh176@users.noreply.github.com>
2018-10-10 17:44:33 +0700

    vuejs

--- VueDotJs/Exercise 8/README.md
+++ VueDotJs/Exercise 8/README.md
@@ -1,6 +1,6 @@
-# vue-cli
+# name
 
-> A Vue.js project
+> description
 
 ## Build Setup
 
--- VueDotJs/Exercise 8/src/components/Server/Servers.vue
+++ VueDotJs/Exercise 8/src/components/Server/Servers.vue
@@ -1,16 +1,29 @@
 <template>
     <div class="col-xs-12 col-sm-6">
         <ul class="list-group">
-            <li
-                    class="list-group-item"
-                    v-for="index in 5">
-                Server #{{ index }}
-            </li>
+          <appServer v-for="server in servers" :server="server"></appServer>
         </ul>
     </div>
 </template>
 
 <script>
+  import Server from './Server.vue'
+
+  export default {
+    data() {
+      return {
+        servers: [
+          { id: 1, status: 'normal'},
+          { id: 2, status: 'critical'},
+          { id: 3, status: 'unknown'},
+          { id: 4, status: 'normal'}
+        ]
+      };
+    },
+    components: {
+      appServer: Server
+    }
+  }
 </script>
 
 <style>
